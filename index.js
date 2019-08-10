bb7c65a97e774da277aa1f65ebfcea3c325d59c1
fuheideMayuyu <xiaguangding@163.com>
2019-08-10 21:23:30 +0800

    内建中间建

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const app = express();
 const logger = require('./logger')
 
 app.use(express.json());
+app.use(express.urlencoded()); // key=value&key=value
 
 app.use(logger)
+
+
+const courses = [
+  {id: 1, name: 'course1'},
+  {id: 2, name: 'course2'},
+  {id: 3, name: 'course3'},
+]
 
 app.use(function(req,res,next) {
   console.log('Authenticating...')
@@ -17,6 +25,14 @@ app.get('/', (req,res) => {
   res.send('Hello World !!!!')
 })
 
+app.post('/api/courses',(req,res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course)
+  res.send(course)
+})
 
 // 设置环境变量
 const port = process.env.PORT || 3000;
